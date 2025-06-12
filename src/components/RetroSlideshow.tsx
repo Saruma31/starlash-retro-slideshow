2633da1e5a4be1167b80a0feaba31ba36a199144
Sek1March <mateo.ruiz@edu.sek1march.ch>
2025-06-12 21:32:04 +0200

    Update RetroSlideshow.tsx

--- src/components/RetroSlideshow.tsx
+++ src/components/RetroSlideshow.tsx
@@ -340,7 +340,7 @@ const RetroSlideshow = () => {
                       key={idx}
                       src={src}
                       alt={`Großes Bild ${idx + 1}`}
-                      className="w-[60%] h-auto object-cover border-4 border-cyan-400 rounded-lg"
+                      className="w-[50%] h-[60vh] object-cover border-4 border-cyan-400 rounded-lg"
                     />
                   ))}
                 </div>
